ff48e1618bb74031a90490078a9e5b5f0142fc88
p0u4a <pouyamiria@gmail.com>
2026-05-20 16:21:55 +0930

    load config at startup

--- src/main.zig
+++ src/main.zig
@@ -3,6 +3,5 @@ const nova = @import("nova");
 
 pub fn main(init: std.process.Init) !void {
     const gpa = std.heap.smp_allocator;
-    const config = nova.Config.fromEnv(init.environ_map);
-    try nova.run(init, gpa, config);
+    try nova.run(init, gpa);
 }
