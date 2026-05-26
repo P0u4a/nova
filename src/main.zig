761f7a3c31e3cc2b064ca0bcc468c4912a3a8b2e
p0u4a <pouyamiria@gmail.com>
2026-05-26 15:52:06 +0930

    disable unexpected_error_tracing

--- src/main.zig
+++ src/main.zig
@@ -1,6 +1,12 @@
 const std = @import("std");
 const nova = @import("nova");
 
+// TODO: This should only be disabled for production since it messes up
+// the TUI buffer
+pub const std_options: std.Options = .{
+    .unexpected_error_tracing = false,
+};
+
 pub fn main(init: std.process.Init) !void {
     const gpa = std.heap.smp_allocator;
     try nova.run(init, gpa);
