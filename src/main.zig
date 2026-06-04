04acd29b0b2769217c9c232d3b09e9620276a959
p0u4a <pouyamiria@gmail.com>
2026-06-04 10:49:34 +0930

    special handle panics for debugging

--- src/main.zig
+++ src/main.zig
@@ -7,6 +7,13 @@ pub const std_options: std.Options = .{
     .unexpected_error_tracing = false,
 };
 
+pub const panic = std.debug.FullPanic(novaPanic);
+
+fn novaPanic(msg: []const u8, first_trace_addr: ?usize) noreturn {
+    std.debug.print("\x1b[?1049l\x1b[?1003l\x1b[?1000l\x1b[?25h\x1b[0m\r\n", .{});
+    std.debug.defaultPanic(msg, first_trace_addr);
+}
+
 pub fn main(init: std.process.Init) !void {
     const gpa = std.heap.smp_allocator;
     try nova.run(init, gpa);
